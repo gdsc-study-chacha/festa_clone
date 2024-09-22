679208b9054d337841771dbb8efee4bfb175d470
chacha <chayeonggeon@gmail.com>
2024-09-22 16:18:19 +0900

    feat. 프로젝트 세팅

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div style={{display: "flex"}}>
+            sex
+        </div>
+    );
+};
+
+export default MainPage;
