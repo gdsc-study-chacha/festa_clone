17e01caa819c27b666cf37c9051d548fe1ee4691
chacha <chayeonggeon@gmail.com>
2024-09-25 08:46:34 +0900

    feat. 헤더 레이아웃

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from "react";
+import Logo1 from "../assets/common/logo1.svg";
+import Logo2 from "../assets/common/logo2.svg";
+import Menu1 from "../assets/common/menu1.svg";
+import Menu2 from "../assets/common/menu2.svg";
+import { useNavigate } from "react-router-dom";
 
-const Header = () => {
+const Header = ({ name, type, onClick }) => {
+  const navigate = useNavigate();
   return (
-    <div>
-      header
+    <div className={"header_container"}>
+      <div className="header_wrapper">
+        <div className="header_Img" onClick={() => navigate("/")}>
+          <img
+            src={type === 1 ? Logo1 : Logo2}
+            alt=""
+            style={{ width: "65px", height: "48px" }}
+          />
+        </div>
+        {name !== undefined && <div className="header_props">{name}</div>}
+        <div className="header_menu_icon" onClick={onClick}>
+          <img
+            src={type === 1 ? Menu1 : Menu2}
+            alt=""
+            style={
+              type === 1
+                ? { width: "40px", height: "40px" }
+                : { width: "24px", height: "24px" }
+            }
+          />
+        </div>
+      </div>
     </div>
   );
 };
