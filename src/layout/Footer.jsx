aeb2aae81b8a6da45e0903aebe6f1b238e1e31ee
chacha <chayeonggeon@gmail.com>
2024-09-25 09:04:32 +0900

    feat. 푸터 레이아웃

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import Logo from "../assets/common/footer_logo.svg";
 
 const Footer = () => {
   return (
-    <div>
-      footer
+    <div className="footer_container">
+      <div>
+        <img src={Logo} alt="" style={{ width: "67px", height: "48px" }} />
+      </div>
+      <div className="footer_word">상명대 천안캠퍼스</div>
+      <div>gdsc</div>
     </div>
   );
 };
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { Outlet} from "react-router-dom"
+import React from "react";
+
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
 
 const Layout = () => {
   return (
     <>
-      <Outlet/>
+      <Outlet />
+      <Footer />
     </>
   );
 };
