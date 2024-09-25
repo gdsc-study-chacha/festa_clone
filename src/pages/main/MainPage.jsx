49aee2b32d1c5afd0f2104a9a3a3d36d8b4ec65d
chacha <chayeonggeon@gmail.com>
2024-09-25 23:57:57 +0900

    feat. branch test

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
+import Header from "../../layout/Header";
 
 const MainPage = () => {
-    return (
-        <div>
-            MainPage
-        </div>
-    );
+  return (
+    <div>
+      <Header type={1} />
+      MainPage
+    </div>
+  );
 };
 
 export default MainPage;
